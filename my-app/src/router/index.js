c8c6c76f5f3856fcd4f0401870fa3eb13edebbff
Jordon R. Roberts <jordonroberts99@yahoo.com>
2020-10-26 15:16:24 -0400

    Route Guard installation

--- my-app/src/router/index.js
+++ my-app/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
 import About from '../components/About.vue'
+import { authGuard } from "../auth/authGuard";
 
 Vue.use(VueRouter)
 
@@ -20,15 +21,14 @@ const routes = [
   {
     path: '/about',
     name: 'About',
-    component: About
+    component: About,
+    beforeEnter: authGuard
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/event/:id',
+    name: 'eventSingle',
+    component: () => import('../components/Home.vue'),
+    beforeEnter: authGuard
   }
 ]
 
